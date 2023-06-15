a555f3e5abc791a49b8a95aa1ba225635ffeb445
LasseJ <lasse.juusela@gmail.com>
2023-06-15 09:39:30 -0300

    6. Token Approvals

--- test/Token.js
+++ test/Token.js
@@ -6,7 +6,7 @@ const tokens = (n) => {
 }
 
 describe('Token', () => {
-	let token, accounts, deployer, receiver
+	let token, accounts, deployer, receiver, exchange
 
 	beforeEach(async () => {
 		const Token = await ethers.getContractFactory('Token')
@@ -15,6 +15,7 @@ describe('Token', () => {
 		accounts = await ethers.getSigners()
 		deployer = accounts[0]
 		receiver = accounts[1]
+		exchange = accounts[2]
 	})
 
 	describe('Deployment', () => {
@@ -60,7 +61,6 @@ describe('Sending Tokens', () => {
 	it('transfers token balances', async () => {	
 		expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900))
 		expect(await token.balanceOf(receiver.address)).to.equal(amount)
-
 	})
 
 	it('emits a Transfer event', async () => {
@@ -87,6 +87,41 @@ describe('Sending Tokens', () => {
   	})
 
   })
+
+})
+
+describe('Approving Tokens', () => {
+	let amount, transaction, result
+
+	beforeEach(async () => {
+		amount = tokens(100)
+		transaction = await token.connect(deployer).approve(exchange.address, amount)
+		result = await transaction.wait()
+	})
+
+	describe('Success', () => {
+		it('allocates an allowance for delegated token spending', async () => {
+			expect(await token.allowance(deployer.address, exchange.address)).to.equal(amount)
+		})
+
+	it('emits an Approval event', async () => {
+		const event = result.events[0]
+		expect(event.event).to.equal('Approval')
+
+		const args = event.args
+		expect(args.owner).to.equal(deployer.address)
+		expect(args.spender).to.equal(exchange.address)
+		expect(args.value).to.equal(amount)
+	  })
+
+
+	})
+
+	describe('Failure', () => {
+		it('rejects invalid spenders', async () => {
+		await expect(token.connect(deployer).approve('0x0000000000000000000000000000000000000000', amount)).to.be.reverted	
+		})
+	})
 
 })
 
